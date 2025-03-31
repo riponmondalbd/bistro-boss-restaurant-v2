9ff46bd0915a5a391d6e73a0697dc3e5d3a5c9a6
riponmondalbd <riponmondal149@gmail.com>
2025-03-31 23:58:43 +0600

    create simple top banner react responsive carousel

--- bistro-boss-client/src/pages/Shared/Navbar/Navbar.jsx
+++ bistro-boss-client/src/pages/Shared/Navbar/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-black/30 fixed z-10 text-white shadow-sm w-7xl">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
