9420820fa1fb40ee92390b003a3b3a87b681434a
riponmondalbd <riponmondal149@gmail.com>
2025-04-01 01:09:21 +0600

    use swipper js to create a category swipper

---
+++ bistro-boss-client/src/pages/Home/Category/Category.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import img1 from "../../../assets/home/slide1.jpg";
+import img2 from "../../../assets/home/slide2.jpg";
+import img3 from "../../../assets/home/slide3.jpg";
+import img4 from "../../../assets/home/slide4.jpg";
+import img5 from "../../../assets/home/slide5.jpg";
+
+const Category = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img1} alt="Slider Image" />
+          <h3 className="text-4xl uppercase text-center text-white -mt-16 pb-16">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt="Slider Image" />
+          <h3 className="text-4xl uppercase text-center text-white -mt-16 pb-16">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} alt="Slider Image" />
+          <h3 className="text-4xl uppercase text-center text-white -mt-16 pb-16">
+            Pizzas
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt="Slider Image" />
+          <h3 className="text-4xl uppercase text-center text-white -mt-16 pb-16">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} alt="Slider Image" />
+          <h3 className="text-4xl uppercase text-center text-white -mt-16 pb-16">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Category;
