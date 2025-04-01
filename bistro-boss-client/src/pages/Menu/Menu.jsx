8dbf493fcf109680de0f7313b0eaab21d227eca7
riponmondalbd <riponmondal149@gmail.com>
2025-04-01 19:34:04 +0600

    implement cover with blur parallax using react parallax

--- bistro-boss-client/src/pages/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import menuImg from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../Home/PopularMenu/PopularMenu";
+import Cover from "../Shared/Cover/Cover";
 
 const Menu = () => {
   return (
     <div>
       <title>Bistro Boss | Menu</title>
-      <h2>Men men menu</h2>
+      <Cover img={menuImg} title={"Our Menu"} />
+      <PopularMenu />
+      <Cover img={menuImg} title={"Our Menu"} />
+      <PopularMenu />
+      <Cover img={menuImg} title={"Our Menu"} />
+      <PopularMenu />
+      <Cover img={menuImg} title={"Our Menu"} />
+      <PopularMenu />
     </div>
   );
 };
