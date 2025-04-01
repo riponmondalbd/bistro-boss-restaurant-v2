92c64ed9334ae8c40c020780873b1cbd98dab6a7
riponmondalbd <riponmondal149@gmail.com>
2025-04-01 23:40:00 +0600

    display different menu category items with cover

--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import menuImg from "../../../assets/menu/banner3.jpg";
+import desertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 import Cover from "../../Shared/Cover/Cover";
+import MenuCategory from "../MenuCategory/MenuCategory";
 
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <title>Bistro Boss | Menu</title>
       <Cover img={menuImg} title={"Our Menu"} />
+      {/* main cover */}
+      <SectionTitle subheading={"Don't miss"} heading={"Today's Offer"} />
+      {/* offered menu items */}
+      <MenuCategory items={offered} />
+      {/* desert menu items */}
+      <MenuCategory items={dessert} title={"Desserts"} coverImg={desertImg} />
+      {/* pizza menu items */}
+      <MenuCategory items={pizza} title={"Pizza"} coverImg={pizzaImg} />
+      {/* salad menu items */}
+      <MenuCategory items={salad} title={"Salads"} coverImg={saladImg} />
+      {/* soup menu items */}
+      <MenuCategory items={soup} title={"Soups"} coverImg={soupImg} />
     </div>
   );
 };
---
+++ bistro-boss-client/src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const MenuCategory = ({ items, title, coverImg }) => {
+  return (
+    <div className="pt-8">
+      {title ? <Cover img={coverImg} title={title} /> : ""}
+      <div className="grid md:grid-cols-2 gap-10 my-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default MenuCategory;
