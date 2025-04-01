e74bfad7bb0ec103f5d28bc9c4a2e8ac97738fca
riponmondalbd <riponmondal149@gmail.com>
2025-04-01 20:08:28 +0600

    create custom hook useMenu to load menu data

---
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import Cover from "../../Shared/Cover/Cover";
+
+const Menu = () => {
+  return (
+    <div>
+      <title>Bistro Boss | Menu</title>
+      <Cover img={menuImg} title={"Our Menu"} />
+    </div>
+  );
+};
+
+export default Menu;
