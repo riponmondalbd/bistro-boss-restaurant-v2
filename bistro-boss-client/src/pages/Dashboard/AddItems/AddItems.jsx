7f9dbec2f6a8d7d14a48b5c9167653306614a86e
riponmondalbd <riponmondal149@gmail.com>
2025-04-07 07:46:17 +0600

    design form using daisy ui and react hook form

--- bistro-boss-client/src/pages/Dashboard/AddItems/AddItems.jsx
+++ bistro-boss-client/src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
 const AddItems = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -19,22 +15,65 @@ const AddItems = () => {
       <title>Bistro Boss | Add Items</title>
       <SectionTitle heading={"add an item"} subheading={"What's New?"} />
       <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("name")} type="text" />
-          <select
-            {...register("category")}
-            defaultValue="Select a Category"
-            className="select"
-          >
-            <option disabled={true}>Select a Category</option>
-            <option value={"salad"}>Salad</option>
-            <option value={"pizza"}>Pizza</option>
-            <option value={"soup"}>Soup</option>
-            <option value={"desert"}>Desert</option>
-            <option value={"drinks"}>Drinks</option>
-          </select>
+        <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+          {/* recipe name  */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Recipe Name*</legend>
+            <input
+              type="text"
+              className="input w-full"
+              placeholder="Recipe Name"
+              {...register("name", { required: true })}
+            />
+          </fieldset>
+          <div className="flex gap-4">
+            {/* category */}
+            <fieldset className="fieldset flex-1">
+              <legend className="fieldset-legend">Category*</legend>
+              <select
+                {...register("category", { required: true })}
+                defaultValue="Select a Category"
+                className="select w-full"
+              >
+                <option disabled={true}>Select a Category</option>
+                <option value={"salad"}>Salad</option>
+                <option value={"pizza"}>Pizza</option>
+                <option value={"soup"}>Soup</option>
+                <option value={"desert"}>Desert</option>
+                <option value={"drinks"}>Drinks</option>
+              </select>
+            </fieldset>
 
-          <input type="submit" />
+            {/* price */}
+            <fieldset className="fieldset flex-1">
+              <legend className="fieldset-legend">Price*</legend>
+              <input
+                type="number"
+                className="input w-full"
+                placeholder="Price"
+                {...register("price", { required: true })}
+              />
+            </fieldset>
+          </div>
+          {/* recipe details */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Recipe Details*</legend>
+            <textarea
+              className="textarea h-24 w-full"
+              placeholder="Recipe Details"
+              {...register("recipe", { required: true })}
+            ></textarea>
+          </fieldset>
+          {/* recipe image */}
+          <input
+            type="file"
+            {...register("image", { required: true })}
+            className="file-input file-input-ghost w-full"
+          />
+
+          <button className="btn">
+            Add Item <FaUtensils />
+          </button>
         </form>
       </div>
     </div>
