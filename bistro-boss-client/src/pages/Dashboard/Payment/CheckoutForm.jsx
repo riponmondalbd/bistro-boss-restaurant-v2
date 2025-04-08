eeef3147df84a5aef5ced72af2b9b710f93abfbd
riponmondalbd <riponmondal149@gmail.com>
2025-04-08 20:35:23 +0600

    create stripe account and use publishable key

--- bistro-boss-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ bistro-boss-client/src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -17,6 +17,17 @@ const CheckoutForm = () => {
     if (card == null) {
       return;
     }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    if (error) {
+      console.log("payment error", error);
+    } else {
+      console.log("payment method", paymentMethod);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -36,7 +47,11 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <button type="submit" disabled={!stripe}>
+      <button
+        className="btn btn-primary btn-sm my-4"
+        type="submit"
+        disabled={!stripe}
+      >
         Pay
       </button>
     </form>
--- bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
+++ bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
@@ -2,16 +2,19 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
-const stripePromise = loadStripe();
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
   return (
     <div>
       <title>Bistro Boss | Payment</title>
       <SectionTitle heading={"Payment"} subheading={"Please pay to eat"} />
       <div>
-        <Elements stripe={stripePromise}></Elements>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
       </div>
     </div>
   );
