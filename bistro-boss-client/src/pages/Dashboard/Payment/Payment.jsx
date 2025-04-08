b4f573a4a622ce8843be0965d4d85a30748460e2
riponmondalbd <riponmondal149@gmail.com>
2025-04-08 20:20:46 +0600

    install stripe, stripe for react and create card element

--- bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
+++ bistro-boss-client/src/pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,17 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 
+// TODO: add publishable key
+const stripePromise = loadStripe();
 const Payment = () => {
   return (
     <div>
       <title>Bistro Boss | Payment</title>
-      <SectionTitle heading={"PAyment"} subheading={"Please pay to eat"} />
+      <SectionTitle heading={"Payment"} subheading={"Please pay to eat"} />
       <div>
-        <h2 className="text-4xl">Teka o pakhi tomi uira uira asho</h2>
+        <Elements stripe={stripePromise}></Elements>
       </div>
     </div>
   );
