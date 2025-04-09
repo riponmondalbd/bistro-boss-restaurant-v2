6bba5b04a79034dea61425a058658c4b628fb095
riponmondalbd <riponmondal149@gmail.com>
2025-04-09 16:43:15 +0600

    create admin dashboard stats api

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -253,6 +253,22 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
+    // stats or analytics
+    app.get("/admin-stats", async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // this is not the best way
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce(
+        (total, payment) => total + payment.price,
+        0
+      );
+
+      res.send({ users, menuItems, orders, revenue });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
