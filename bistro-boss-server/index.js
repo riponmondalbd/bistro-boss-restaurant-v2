a8bdbab1b11e8c8a9e442de74b17114c9df7870e
riponmondalbd <riponmondal149@gmail.com>
2025-04-09 19:12:14 +0600

    display custom bar chart and custom pie chart

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -285,7 +285,7 @@ async function run() {
     });
 
     // using aggregate pipeline
-    app.get("/order-stats", async (req, res) => {
+    app.get("/order-stats", verifyToken, verifyAdmin, async (req, res) => {
       const result = await paymentCollection
         .aggregate([
           {
@@ -307,6 +307,14 @@ async function run() {
               _id: "$menuItems.category",
               quantity: { $sum: 1 },
               revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              category: "$_id",
+              quantity: "$quantity",
+              revenue: "$revenue",
             },
           },
         ])
